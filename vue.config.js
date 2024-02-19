85143f7256ee75bba9bd26f62a381946a21ed243
Valentin Shevchenko <valwel@bk.ru>
2024-02-19 14:15:26 +0300

    Добавлен dist

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  base: "/weather/",
+  base: "/myWeather/",
 })
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/chat/" : "/",
